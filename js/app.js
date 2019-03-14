567884ac8aa98a3cc4e1d5fe4eb709c970ca0da9
BuildTools <unconfigured@null.spigotmc.org>
2019-03-14 14:07:34 +0100

    liste de base fini

--- js/app.js
+++ js/app.js
@@ -4,8 +4,13 @@ var ComponentList = {
         <li>
             <input type="checkbox">
             {{ elem.id + " " + elem.text }}
-            <button @click="alert('1')">X</button>
-        </li>`
+            <button @click="suppElmtComp(elem.id)">X</button>
+        </li>`,
+    methods: {
+        suppElmtComp(index) {
+            this.$parent.suppElmt(index);
+        }
+    }
 }
 
 
@@ -15,18 +20,20 @@ var app = new Vue({
         'list': ComponentList
     },
     data: {
-        counterID : 4,
+        counterID : 3,
         list: [
-            {id: 1, text: 'Liste 1'},
-            {id: 2, text: 'Liste 2'},
-            {id: 3, text: 'Liste 3'}
+            {id: 0, text: 'Liste 1'},
+            {id: 1, text: 'Liste 2'},
+            {id: 2, text: 'Liste 3'}
         ]
     },
     methods: {
-        suppElmt(index) {
-            console.log(index)
-            // this.items.splice(index, 1);
-            // Vue.delete(this.items, index);
+        suppElmt(id) {
+            this.list.forEach((elmt, index) => {
+                if(elmt.id === id) {
+                    Vue.delete(this.list, index);
+                }
+            });
         }
     },
     computed: {
